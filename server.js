977103c3925f527c5938b3d6f5d205cbb2cf96a3
liatgin <liatginosar@gmail.com>
2017-12-09 12:59:42 +0200

    Working without ajax requests. Have to add ajax requests for each column change/request.

--- server.js
+++ server.js
@@ -29,46 +29,76 @@ server.get('/', function(req, res){
 });
 
 server.get('/hi/bi', function(req, res){
-	console.log(db)
+	//console.log(db)
 	//res.sendFile(path.join(__dirname +'/connect.js'));
-	var chosen_column = "reason for unemployment"
-	//db.all("PRAGMA table_info(census_learn_sql)", function (err, rows){console.log(rows)});
-    db.all('SELECT "' + chosen_column + '", COUNT(*), AVG(age) FROM census_learn_sql GROUP BY "' + chosen_column + '" ORDER BY COUNT(*) DESC LIMIT 100', function(err, rows) {
-		console.log(rows)
-		var html = ""	
-		html+= "<html>"
-		html+=	"<head>"
-		html+=	"<style>"
-		html+=	"table, th, td {"
-		html+=	    "border: 1px solid black;"
-		html+=	    "padding: 5px;"
-		html+=	"}"
-		html+=	"table {"
-		html+=	    "border-spacing: 15px;"
-		html+=	"}"
-		html+=	"</style>"
-		html+=	"</head>"
-		html+=	"<body>"
-		html+=	"<table style=width:100%>"
-		html+=	  "<tr>"
-		html+=	    "<th>Values</th>"
-		html+=	    "<th>Average Age</th>"
-		html+=		"<th>Count</th>"
-		html+=	  "</tr>"
-		
-		rows.forEach((row) => {
-			html+=	  "<tr>"
-			html+=	    "<td> "+row[String(chosen_column)]+"</td>"
-			html+=	    "<td> "+row['AVG(age)']+"</td>"
-			html+=	    "<td> "+row['COUNT(*)']+"</td>"
-			html+=	  "</tr>"
-		});	  
-		html+=	"</table>"
-		html+=	"</body>"
-		html+= "</html>"
-	   	res.send(html);
-      });
+	var chosen_column = "capital gain"
+    var sql_final_table = 'SELECT "' + chosen_column + '", COUNT(*), AVG(age) FROM census_learn_sql GROUP BY "' + chosen_column + '" ORDER BY COUNT(*) DESC LIMIT 100'
+	var sql_rows_on_db = 'SELECT COUNT(*) FROM census_learn_sql'
+	var sql_unique_values_db = 'SELECT COUNT(DISTINCT"' + chosen_column + '") FROM census_learn_sql'
+	//var sql_clipped_rows = 'SELECT SUM(COUNT(*)) FROM FINAL_TABLE'
+	db.all(sql_final_table, function(err, rows1){
+		// console.log(rows1.length);
+		db.all(sql_rows_on_db, function(err, rows2){
+			//console.log(rows2[0]['COUNT(*)']);
+			db.all(sql_unique_values_db, function(err, rows3){
+				console.log(rows3[0]['COUNT(DISTINCT"'+chosen_column+'")']);
+				var numOfUniqueVals = rows3[0]['COUNT(DISTINCT"'+chosen_column+'")'];
+				var hiddenValues = numOfUniqueVals - rows1.length;
+				if (hiddenValues < 0)
+				{	
+					hiddenValues = 0
+				}		
+
+				var sumOfRows = 0
+				rows1.forEach((row) => {
+					sumOfRows += row['COUNT(*)'];
+				});	
+				var numOfRowsInDb = rows2[0]['COUNT(*)'];
+				var clippedRows = numOfRowsInDb - sumOfRows;
+				if (clippedRows < 0)
+				{
+					clippedRows = 0;
+				}
+				var html = ""	
+				html+= "<html>"
+				html+=	"<head>"
+				html+=	"<style>"
+				html+=	"table, th, td {"
+				html+=	    "border: 1px solid black;"
+				html+=	    "padding: 5px;"
+				html+=	"}"
+				html+=	"table {"
+				html+=	    "border-spacing: 15px;"
+				html+=	"}"
+				html+=	"</style>"
+				html+=	"</head>"
+				html+=	"<body>"
+				html+= "<div> CLIPPED ROWS: " + clippedRows + "</div>"
+				html+= "<div> HIDDEN VALUES: " + hiddenValues + "</div>"
+				html+=	"<table style=width:100%>"
+				html+=	  "<tr>"
+				html+=	    "<th>Values</th>"
+				html+=	    "<th>Average Age</th>"
+				html+=		"<th>Count</th>"
+				html+=	  "</tr>"
+				
+				rows1.forEach((row) => {
+					html+=	  "<tr>"
+					html+=	    "<td> "+row[String(chosen_column)]+"</td>"
+					html+=	    "<td> "+row['AVG(age)']+"</td>"
+					html+=	    "<td> "+row['COUNT(*)']+"</td>"
+					html+=	  "</tr>"
+				});	  
+				html+=	"</table>"
+				html+=	"</body>"
+				html+= "</html>"
+			   	res.send(html);
+			});
+		});
+	});
 });
+
+
 // Server static files
 server.use('/api', apiRouter);
 server.use(express.static('public'));
@@ -77,4 +107,3 @@ server.use(express.static('public'));
 server.listen(config .port, () => {
 	console.info('express listening on port', config.port);
 });
-
